81dffbb8fb10727c803cb44fc0416ccf383a7010
erwanlbp <erwan.le-batard-poles@setec.com>
2020-01-29 23:08:38 +0100

    update deployment scripts

--- deploy_web.sh
+++ deploy_web.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-deploy_mesage=$1
+deploy_mesage=to_change
 
-firebase deploy --only hosting --message $deploy_mesage
+## Check that there is the correct number of args
+case ${#} in
+	1)
+		deploy_mesage=--message $1
+		;;
+	*)
+	  echo ""
+		echo "one argument required : the deployment message"
+		echo ""
+		exit 3
+		;;
+esac
+
+firebase deploy --only hosting $deploy_mesage
 
 exit 0
