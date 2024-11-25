fd19e01fbcb21613788880c0ef54e4de5048858b
Erwan Le Batard--Polès <erwan.lbp@gmail.com>
2024-11-25 19:28:02 +0100

    restart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { RoutePathConstants } from './constants/route.constants';
-import { AuthService } from './services/auth.service';
-import { Observable } from 'rxjs';
-import * as jsonPackage from './../../package.json';
-import * as moment from 'moment';
-import { UtilsService } from "./services/utils.service";
-import { AccountService } from './services/account.service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: 'app.component.html',
-    styleUrls: ['app.component.scss']
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-
-    connected$: Observable<boolean>;
-    version: string = jsonPackage.version;
-
-    pages = [
-        {title: 'Comment jouer ?', url: RoutePathConstants.HOME, icon: 'help'},
-        {title: 'Jeu', url: RoutePathConstants.PLAY, icon: 'logo-game-controller-b'},
-        {title: 'Historique', url: RoutePathConstants.GAMES_HISTORY, icon: 'stats'},
-        {title: 'Scores', url: RoutePathConstants.SCORES, icon: 'podium'},
-        {title: 'Compte', url: RoutePathConstants.ACCOUNT, icon: 'contact'},
-    ];
-
-    constructor(
-        private platform: Platform,
-        private splashScreen: SplashScreen,
-        private statusBar: StatusBar,
-        private authService: AuthService,
-        private utilsService: UtilsService,
-        private accountService: AccountService,
-    ) {
-        this.initializeApp();
-    }
-
-    initializeApp() {
-        moment.locale('fr-FR');
-        this.platform.ready().then(() => {
-            this.statusBar.styleDefault();
-            this.splashScreen.hide();
-        });
-    }
-
-    ngOnInit() {
-        this.connected$ = this.authService.isConnected$();
-    }
-
-    logout() {
-        this.authService.logout();
-    }
-
-    login() {
-        this.authService.login()
-            .then(user => this.accountService.updateAccountEmail(user.user.email))
-            .then(() => this.utilsService.showToast('Connexion réussie'))
-            .catch(() => this.utilsService.showToast('Echec de la connexion'));
-    }
+export class AppComponent {
+  title = 'calculis';
 }
--- src/app/model/user-game.class.ts
+++
@@ -1,3 +0,0 @@
-export class UserGame {
-    userGameId: string;
-}
