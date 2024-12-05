32a68514d0f35f49c193058c8c422c6f2abf202c
Alex Vacheret <alex.vacheret@free.fr>
2024-12-05 18:18:54 +0100

    debut menu mobile

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,5 +1,5 @@
 export const MENU_ITEMS = [
-  {link: '/home', icon: 'home', label: 'Home',},
-  {link: '/games', icon: '123', label: 'Games',},
-  {link: '/account', icon: 'person', label: 'Account',},
+  {link: 'home', icon: 'home', label: 'Home'},
+  {link: 'games', icon: 'videogame_asset', label: 'Games'},
+  {link: 'account', icon: 'person', label: 'Account'},
 ]
--- src/app/components/menu/mobile-menu/mobile-menu.component.ts
+++ src/app/components/menu/mobile-menu/mobile-menu.component.ts
@@ -1,12 +1,11 @@
 import { Component, inject, Signal } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { NgForOf, NgIf } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { MENU_ITEMS } from '../menu';
 import { MatTooltip } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { UtilsService } from '../../../services/utils.service';
 import { AuthService } from '../../../services/auth-service';
 import { toSignal } from '@angular/core/rxjs-interop';
 
@@ -31,14 +30,18 @@ export class MobileMenuComponent {
   menuItems = MENU_ITEMS;
 
   private authService = inject(AuthService);
+  private router = inject(Router);
   connected: Signal<boolean>
 
-
   constructor() {
-    this.connected = toSignal(this.authService.isConnected$(), { initialValue: false })
+    this.connected = toSignal(this.authService.isConnected$(), {initialValue: false})
   }
 
   login() {
     this.authService.login()
+  }
+
+  navigateTo(itemLink: string) {
+    this.router.navigate(['/', itemLink]);
   }
 }
